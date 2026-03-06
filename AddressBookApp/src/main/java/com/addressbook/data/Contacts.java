53d612d59e51ea34aec8ac69131ee6af85a05a0a
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-03-06 15:13:10 +0530

    [MohdMeraaz] : added AddressBookApp

---
+++ AddressBookApp/src/main/java/com/addressbook/data/Contacts.java
@@ -0,0 +1,73 @@
+package com.addressbook.data;
+
+public class Contacts {
+	String firstName;
+	String lastName;
+	String Address;
+	String city;
+	String state;
+	int zip;
+	int phoneNumber;
+	String email;
+	public Contacts(String firstName, String lastName, String address, String city, String state, int zip,
+			int phoneNumber, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		Address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
